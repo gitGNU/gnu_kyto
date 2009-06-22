a4e54d1b6c153d8f44f783e079141f019c65dfcd
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2009-06-22 21:09:23 +0200

    Changed the proxies stored in DDBB for useful data.

---
+++ KytoTries/src/ServerLocal/PlanI.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package ServerLocal;
+
+import Ice.Current;
+import Ice.Identity;
+import KytoTries.TaskPrx;
+import KytoTries._PlanDisp;
+
+/**
+ * @author miguel
+ *
+ */
+public class PlanI extends _PlanDisp
+{
+
+	/**
+    * 
+    */
+   private static final long serialVersionUID = 1L;
+
+	@Override
+   public TaskPrx searchTask(Identity id, Current __current)
+   {
+	   // TODO Auto-generated method stub
+	   return null;
+   }
+
+   
+   
+}
